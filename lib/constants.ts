04ae4f9d7d728bf7680830e879167d5091dbd178
Abraham Yusuf <1703020028@students.unis.ac.id>
2022-06-02 01:04:01 +0700

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,29 @@
+// social base url
+export const TwitterURL = 'https://twitter.com'
+
+// Global Data
+export const SITE_URL = 'https://catatan-abraham-yusuf.vercel.app'
+export const SITE_NAME = 'Catatan Abraham Yusuf'
+export const SITE_LOGO =
+  'https://res.cloudinary.com/abrahamyusufdevimeilani/image/upload/v1632691144/Catatan_Abraham_Yusuf_8621a41f9e.png'
+
+export const SOCIAL_USERNAMES = {
+  twitter: 'bram0511',
+  instagram: 'ab129h9m',
+  facebook: 'AbrahamYusuf1105',
+  youtube: 'CatatanAbrahamYusuf',
+  linkedin: 'abraham-yusuf',
+}
+
+// Default SEO
+export const SEO_DESCRIPTION =
+  'Blog Tempat Berbagi Cerita Dan Pengalaman Teknologi Era 4.0. '
+
+export const OG_IMAGE = {
+  large: {
+    url:
+      'https://res.cloudinary.com/abrahamyusufdevimeilani/image/upload/v1632691144/Catatan_Abraham_Yusuf_8621a41f9e.png',
+    width: 1200,
+    height: 627,
+  },
+}
